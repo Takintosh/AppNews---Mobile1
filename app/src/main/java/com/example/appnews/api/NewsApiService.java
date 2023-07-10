4388e5cc33a4a1d0b04af7fd964e9f1e0f7ea76d
Amilcar Santana <takintosh@gmail.com>
2023-07-10 17:35:17 -0300

    API Service update

--- app/src/main/java/com/example/appnews/api/NewsApiService.java
+++ app/src/main/java/com/example/appnews/api/NewsApiService.java
@@ -4,11 +4,14 @@ import com.example.appnews.models.News;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface NewsApiService {
+    @Headers("X-Api-Key: cb698ff3d2584e8288367228f998a554")
     @GET("top-headlines")
     Call<News> getTopHeadlines(
+            @Query("category") String category,
             @Query("country") String country,
             @Query("apiKey") String apiKey
     );
